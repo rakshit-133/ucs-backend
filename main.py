b2155db048d0ae6bccc03f85cc53269af4d17cc4
Rakshit Aggarwal <rakshitaggarwal133@gmail.com>
2025-11-17 14:08:27 +0530

    health check added

--- main.py
+++ main.py
@@ -15,6 +15,11 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"status": "Backend is running on Render 🚀"}
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
 # --- CORS Configuration ---
 frontend_url = os.environ.get('FRONTEND_URL', 'http://localhost:5173')
 
